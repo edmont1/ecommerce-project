d8453bf8e8f32324238f14e1042662e183f0fa1f
Eduardo <henridudu@gmail.com>
2022-09-21 05:04:51 -0300

    header and banner

---
+++ projeto-senai/pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Anton&family=Bungee+Spice&family=Fontdiner+Swanky&family=Londrina+Outline&family=Major+Mono+Display&family=Mouse+Memoirs&family=Nabla&family=New+Rocker&family=Nosifer&family=Permanent+Marker&family=Raleway&family=Rampart+One&family=Reggae+One&family=Rubik+Dirt&family=Rubik+Iso&family=Sen:wght@800&family=Waiting+for+the+Sunrise&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
